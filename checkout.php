d76106ee61ff04c25a05cd052856b01e0554584a
Pollyana Ferreira <pollyana.ferreiralima@gmail.com>
2023-11-20 23:26:15 +0000

    tirei a descrição duplicada

--- checkout.php
+++ checkout.php
@@ -97,8 +97,7 @@ if (isset($_GET['id'])) {
     <div class="col-md-5">
         <h1 class="font-weight-normal normal"><?= $produto['nome_produto']; ?></h1>
         <p class="font-weight-normal mt-5">
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+        <?= $produto['descrição']; ?>
         <h1>
             <b>R$ <?= number_format($produto['preço'], 2, ',', '.'); ?></b>
         </h1>
@@ -109,15 +108,8 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
     </div>
   </div>
 
-  <div class="row mt-3">
-        <div class="col-md-12">
-            <h3>Descrição</h3>
-            <p class="font-weight-normal mt-2">
-                <?= $produto['descrição']; ?>
-            <hr class="w-100">
-        </div>
-    </div>
-    <div class="row">
+
+    <div class="row mt-3">
         <div class="col-md-12">
             <h3>Detalhes</h3>
             <p class="font-weight-normal mt-2">
